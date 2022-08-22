816882bc42f259cf6e6cdf68472166a44006d621
KevalSarvadhi <keval.gajera@sarvadhi.com>
2022-08-22 12:38:54 +0530

    update faqs extra page

--- lib/src/views/extra_pages/faqs.dart
+++ lib/src/views/extra_pages/faqs.dart
@@ -11,28 +11,28 @@ class FAQs extends StatefulWidget {
 }
 
 class _FAQsState extends State<FAQs> {
-  final titleList = [
+  final _dataGeneralList = [
     "The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental.",
     "Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar",
     "If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple",
     "Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar",
   ];
 
-  final dataList = [
+  final _generalList = [
     "What is Lorem Ipsum?",
     "Where does it come from?",
     "Why do we use it?",
     "Where can I get some?",
   ];
 
-  final titleList1 = [
+  final _dataPricingNplanList = [
     "The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental.",
     "Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar",
     "If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple",
     "Everyone realizes why a new common language would be desirable one could refuse to pay expensive translators. Toachieve this, it would be necessary to have uniform grammar",
   ];
 
-  final dataList1 = [
+  final _pricingNplanList = [
     "Where can I get some?",
     "Why do we use it?",
     "Where does it come from?",
@@ -41,195 +41,209 @@ class _FAQsState extends State<FAQs> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Responsive.isMobile(context) || Responsive.isTablet(context)
-                ? SizedBox(
-                    width: MediaQuery.of(context).size.width * .9,
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 20, right: 20, bottom: 20, top: 60),
-                        child: Column(
-                          children: [
-                            const Icon(
-                              Icons.message_outlined,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Card(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 60),
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.message_outlined,
+                  color: ColorConst.primary,
+                ),
+                FxBox.h20,
+                const Text(
+                  "Can't find what you are looking for?",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                FxBox.h12,
+                const Text(
+                  "If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14),
+                ),
+                FxBox.h20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FxButton(
+                      borderRadius: 4,
+                      onPressed: () {},
+                      text: "Email Us",
+                    ),
+                    FxBox.w8,
+                    FxButton(
+                      borderRadius: 4,
+                      onPressed: () {},
+                      text: "Send us a tweet",
+                      buttonType: ButtonType.success,
+                    ),
+                  ],
+                ),
+                FxBox.h44,
+                Responsive.isMobile(context) || Responsive.isTablet(context)
+                    ? Column(
+                        children: [
+                          const ListTile(
+                            title: Text(
+                              "General Questions",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            leading: Icon(
+                              Icons.book_online_rounded,
                               color: ColorConst.primary,
                             ),
-                            FxBox.h20,
-                            const Text(
-                              "Can't find what you are looking for?",
+                          ),
+                          _expansionList(_dataGeneralList, _generalList),
+                          FxBox.h44,
+                          const ListTile(
+                            title: Text(
+                              "Pricing & Plans",
                               style: TextStyle(
-                                fontSize: 20,
                                 fontWeight: FontWeight.bold,
-                                color: ColorConst.primary,
-                              ),
-                            ),
-                            FxBox.h12,
-                            const Text(
-                              "If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.",
-                              style: TextStyle(fontSize: 14),
-                            ),
-                            FxBox.h20,
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                FxButton(
-                                  borderRadius: 4,
-                                  onPressed: () {},
-                                  text: "Email Us",
-                                ),
-                                FxBox.w8,
-                                FxButton(
-                                  borderRadius: 4,
-                                  onPressed: () {},
-                                  text: "Send us a tweet",
-                                  buttonType: ButtonType.success,
-                                ),
-                              ],
-                            ),
-                            FxBox.h44,
-                            const ListTile(
-                              title: Text(
-                                "General Questions",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                              leading: Icon(
-                                Icons.book_online_rounded,
-                                color: ColorConst.primary,
-                              ),
-                            ),
-                            _expansionList(titleList, dataList),
-                            FxBox.h44,
-                            const ListTile(
-                              title: Text(
-                                "Pricing & Plans",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                              leading: Icon(
-                                Icons.book_online_rounded,
-                                color: ColorConst.primary,
-                              ),
-                            ),
-                            _expansionList(titleList1, dataList1),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                : Card(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 88,
-                        right: 88,
-                        top: 52,
-                      ),
-                      child: Column(
-                        children: [
-                          const Icon(Icons.message_outlined,
-                              color: ColorConst.primary),
-                          FxBox.h20,
-                          const Text(
-                            "Can't find what you are looking for?",
-                            style: TextStyle(
                                 fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Color((0XFF5B626B))),
-                          ),
-                          FxBox.h12,
-                          const SizedBox(
-                            width: 550,
-                            child: Text(
-                              "If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.",
-                              style: TextStyle(fontSize: 14),
-                              textAlign: TextAlign.center,
+                              ),
+                            ),
+                            leading: Icon(
+                              Icons.book_online_rounded,
+                              color: ColorConst.primary,
                             ),
                           ),
-                          FxBox.h20,
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              FxButton(
-                                borderRadius: 4,
-                                onPressed: () {},
-                                text: "Email Us",
-                                color: ColorConst.primary,
-                              ),
-                              FxBox.w8,
-                              FxButton(
-                                borderRadius: 4,
-                                onPressed: () {},
-                                text: "Send us a tweet",
-                                color: ColorConst.darkGreen,
-                              ),
-                            ],
-                          ),
-                          FxBox.h64,
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    const ListTile(
-                                      title: Text(
-                                        "General Questions",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      leading: Icon(
-                                        Icons.book_online_rounded,
-                                        color: ColorConst.primary,
-                                      ),
-                                    ),
-                                    FxBox.h16,
-                                    _expansionList(titleList, dataList),
-                                  ],
-                                ),
-                              ),
-                              FxBox.w40,
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    const ListTile(
-                                      title: Text(
-                                        "Pricing & Plans",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      leading: Icon(
-                                        Icons.book_online_rounded,
-                                        color: ColorConst.primary,
-                                      ),
-                                    ),
-                                    FxBox.h16,
-                                    _expansionList(titleList1, dataList1),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
+                          _expansionList(
+                              _dataPricingNplanList, _pricingNplanList),
                         ],
-                      ),
-                    ),
-                  ),
-          ],
+                      )
+                    : SizedBox()
+              ],
+            ),
+          ),
+        )
+        // : Card(
+        //     child: Padding(
+        //       padding: const EdgeInsets.only(
+        //         left: 88,
+        //         right: 88,
+        //         top: 52,
+        //       ),
+        //       child: Column(
+        //         children: [
+        //           const Icon(Icons.message_outlined,
+        //               color: ColorConst.primary),
+        //           FxBox.h20,
+        //           const Text(
+        //             "Can't find what you are looking for?",
+        //             style: TextStyle(
+        //               fontSize: 20,
+        //               fontWeight: FontWeight.bold,
+        //             ),
+        //           ),
+        //           FxBox.h12,
+        //           const SizedBox(
+        //             width: 550,
+        //             child: Text(
+        //               "If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.",
+        //               style: TextStyle(fontSize: 14),
+        //               textAlign: TextAlign.center,
+        //             ),
+        //           ),
+        //           FxBox.h20,
+        //           Row(
+        //             mainAxisAlignment: MainAxisAlignment.center,
+        //             children: [
+        //               FxButton(
+        //                 borderRadius: 4,
+        //                 onPressed: () {},
+        //                 text: "Email Us",
+        //                 color: ColorConst.primary,
+        //               ),
+        //               FxBox.w8,
+        //               FxButton(
+        //                 borderRadius: 4,
+        //                 onPressed: () {},
+        //                 text: "Send us a tweet",
+        //                 color: ColorConst.darkGreen,
+        //               ),
+        //             ],
+        //           ),
+        //           FxBox.h64,
+        //           Row(
+        //             crossAxisAlignment: CrossAxisAlignment.start,
+        //             children: [
+        //               Expanded(
+        //                 child: Column(
+        //                   children: [
+        //                     const ListTile(
+        //                       title: Text(
+        //                         "General Questions",
+        //                         style: TextStyle(
+        //                           fontWeight: FontWeight.bold,
+        //                           fontSize: 20,
+        //                         ),
+        //                       ),
+        //                       leading: Icon(
+        //                         Icons.book_online_rounded,
+        //                         color: ColorConst.primary,
+        //                       ),
+        //                     ),
+        //                     FxBox.h16,
+        //                     _expansionList(_dataGeneralList, _generalList),
+        //                   ],
+        //                 ),
+        //               ),
+        //               FxBox.w40,
+        //               Expanded(
+        //                 child: Column(
+        //                   children: [
+        //                     const ListTile(
+        //                       title: Text(
+        //                         "Pricing & Plans",
+        //                         style: TextStyle(
+        //                           fontWeight: FontWeight.bold,
+        //                           fontSize: 20,
+        //                         ),
+        //                       ),
+        //                       leading: Icon(
+        //                         Icons.book_online_rounded,
+        //                         color: ColorConst.primary,
+        //                       ),
+        //                     ),
+        //                     FxBox.h16,
+        //                     _expansionList(
+        //                         _dataPricingNplanList, _pricingNplanList),
+        //                   ],
+        //                 ),
+        //               )
+        //             ],
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+      ],
+    );
+  }
+
+  Widget _commonHeading(String text) {
+    return ListTile(
+      title: Text(
+        text,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
         ),
+      ),
+      leading: const Icon(
+        Icons.book_online_rounded,
+        color: ColorConst.primary,
       ),
     );
   }
