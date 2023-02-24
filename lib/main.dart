56e85d29ee8c09b122d47714576889562ca61beb
sarvadhi <abhishek.patel@sarvadhi.com>
2023-02-24 17:45:22 +0530

    routemangedafterrefresh

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,17 @@ import 'package:window_size/window_size.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  // js.context.callMethod('history.pushState', [null, null, '/newpage']);
+
+  // // replace the current history entry when navigating within the same page
+  // js.context.callMethod('history.replaceState', [null, null, '/currentpage']);
+
+  // // listen for changes to the browser's navigation state
+  // js.context['window'].addEventListener(
+  //   'popstate',
+  //   (event) {},
+  // );
+
   WidgetsFlutterBinding.ensureInitialized();
   if (TargetPlatform.windows == defaultTargetPlatform ||
       TargetPlatform.macOS == defaultTargetPlatform) {
@@ -13,6 +24,7 @@ void main() {
     setWindowMaxSize(Size.infinite);
   }
   usePathUrlStrategy();
+
   runApp(
     const MyApp(),
   );
